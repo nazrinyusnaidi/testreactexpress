43bd14cf9513c8b749f0e30857f1585198021944
nazrinyusnaidi <nazrin.yusnaidi@gmail.com>
2022-06-02 18:12:39 +0800

    new commit for new method to query data

--- src/controllers/dataQuery.controller.js
+++ src/controllers/dataQuery.controller.js
@@ -1,17 +1,33 @@
 const dataQuery = require('../services/dataQuery.services');
+const db = require('../services/db');
+const helper = require('../utils/helper.utils');
+const statement = require('../services/queriesList');
 
+
+//example of router > controller > query list(see: services/queriesList.js)
+//directly using db query services in controller level
+//queriesList.js only contains plain DB query statements
 async function selectAll(req, res, next) {
+    var rows = {};
     try {
-      res.json(await dataQuery.selectAll());
+      rows = res.json(await db.query(statement.select)); 
     } catch (err) {
       console.error(`Error while getting programming languages `, err.message);
       next(err);
     }
+
+    const data = helper.emptyOrRows(rows);
+    
+      return {
+        data
+      }
   }
 
+  //example of router > controller > query services(using function)
+  //db query services is used in functions in services/dataQuery.services.js
   async function createRow(req, res, next) {
     try {
-      res.json(await dataQuery.createRow(req.body));
+      res.json(await dataQuery.createRow(req.body)); 
     } catch (err) {
       console.error(`Error while creating new row`, err.message);
       next(err);
---
+++ src/services/queriesList.js
@@ -0,0 +1,6 @@
+const select = "SELECT * FROM programme";
+// const insert = `INSERT INTO yearsemester(yearsem_id, yearsem_year, yearsem_sem, yearsem_type) VALUES (${req.body.id},${req.body.year},${req.body.sem},"${req.body.semType}")`;
+
+module.exports = {
+    select
+}
