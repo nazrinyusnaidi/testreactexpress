5e2d7c291f7278d6297ab91f70e2fa7fd700c055
nazrinyusnaidi <nazrin.yusnaidi@gmail.com>
2022-06-17 09:26:25 +0800

    Master backup without Dockerize

--- src/configs/db.config.js
+++ src/configs/db.config.js
@@ -1,14 +1,14 @@
 
-const Pool = require('pg').Pool
-const pool = new Pool({
-  user: 'your-username',
-  host: 'your-db-hosting-server',
-  database: 'your-db-name',
-  password: 'your-db-password',
-  port: 5432,
-  ssl: { 
-    rejectUnauthorized: false 
-  }
-})
-pool.connect();
-module.exports = pool;
+// const Pool = require('pg').Pool
+// const pool = new Pool({
+//   user: 'your-username',
+//   host: 'your-db-hosting-server',
+//   database: 'your-db-name',
+//   password: 'your-db-password',
+//   port: 5432,
+//   ssl: { 
+//     rejectUnauthorized: false 
+//   }
+// })
+// pool.connect();
+// module.exports = pool;
