a00ea62fea80f93a3ca12cb8ae3d6143124d1e4b
nazrinyusnaidi <nazrin.yusnaidi@gmail.com>
2022-06-27 15:52:18 +0800

    Installed Sinon for API request unit testing mock data

--- src/controllers/dataQuery.controller.test.js
+++ src/controllers/dataQuery.controller.test.js
@@ -1,6 +1,31 @@
+//const { DESCRIBE } = require("sequelize/types/query-types");
 const controller = require("./dataQuery.controller");
+const sinon = require('sinon');
+
+const mockRequest = () => {
+  return {
+    users: []
+  };
+};
+
+const mockResponse = () => {
+  const res = {};
+  res.status = sinon.stub().returns(res);
+  res.json = sinon.stub().returns(res);
+  return res;
+};
 
 test('adds 1 + 2 to equal 3', () => {
     expect(controller.testUnit(1, 2)).toBe(3);
   });
 
+
+
+describe('API testing', () => {
+  test('test GET request', async () => {
+    const req = mockRequest();
+    const res = mockResponse();
+    await controller.selectAll(req, res);
+    expect(res.json).toBeDefined();
+  });
+});
