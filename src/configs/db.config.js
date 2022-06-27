398b3f402d83d88481f4448e96d541a9c89921d4
nazrinyusnaidi <nazrin.yusnaidi@gmail.com>
2022-06-27 09:41:57 +0800

    DB config updated

--- src/configs/db.config.js
+++ src/configs/db.config.js
@@ -1,10 +1,10 @@
 
 const Pool = require('pg').Pool
 const pool = new Pool({
-  user: 'luvatjzqxanslg',
-  host: 'ec2-34-225-159-178.compute-1.amazonaws.com',
-  database: 'd2jlflrgkqf5op',
-  password: 'd9713f279f4b7e85486eab5d15299c386a05548f3ccdefb53373798c72829641',
+  user: 'username',
+  host: 'hostname',
+  database: 'dbname',
+  password: 'dbpassword',
   port: 5432,
   ssl: { 
     rejectUnauthorized: false 
